4fece87e3a2c9ac322335e32e80873ac5557e33a
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-08-20 12:54:44 +0200

    Add tests for TextColorMatcher.

---
+++ espresso-core-utils/src/androidTest/java/com/vanniktech/espresso/core/utils/TextColorMatcherTest.java
@@ -0,0 +1,55 @@
+package com.vanniktech.espresso.core.utils;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import com.vanniktech.espresso.core.utils.test.R;
+import junit.framework.AssertionFailedError;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+
+import static android.graphics.Color.GREEN;
+import static android.graphics.Color.RED;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.vanniktech.espresso.core.utils.TextColorMatcher.withTextColor;
+import static com.vanniktech.espresso.core.utils.TextColorMatcher.withTextColorRes;
+import static com.vanniktech.espresso.core.utils.TextColorMatcherActivity.VIEW_ID;
+
+@RunWith(AndroidJUnit4.class) public final class TextColorMatcherTest {
+  @Rule public final ExpectedException expectedException = ExpectedException.none();
+
+  @Rule public final ActivityTestRule<TextColorMatcherActivity> activityTestRule = new ActivityTestRule<>(TextColorMatcherActivity.class);
+
+  @Test public void withTextColorResMatches() {
+    onView(withId(VIEW_ID)).check(matches(withTextColorRes(R.color.red)));
+  }
+
+  @Test public void withTextColorMatches() {
+    onView(withId(VIEW_ID)).check(matches(withTextColor(RED)));
+  }
+
+  @Test public void withTextColorStringMatches() {
+    onView(withId(VIEW_ID)).check(matches(withTextColor("#ff0000")));
+  }
+
+  @Test public void withTextColorResDoesNotMatch() {
+    expectedException.expect(AssertionFailedError.class);
+    expectedException.expectMessage("'with text color: ' doesn't match the selected view.");
+    onView(withId(VIEW_ID)).check(matches(withTextColorRes(R.color.green)));
+  }
+
+  @Test public void withTextColorDoesNotMatch() {
+    expectedException.expect(AssertionFailedError.class);
+    expectedException.expectMessage("'with text color: ' doesn't match the selected view.");
+    onView(withId(VIEW_ID)).check(matches(withTextColor(GREEN)));
+  }
+
+  @Test public void withTextColorStringDoesNotMatch() {
+    expectedException.expect(AssertionFailedError.class);
+    expectedException.expectMessage("'with text color: ' doesn't match the selected view.");
+    onView(withId(VIEW_ID)).check(matches(withTextColor("#00ff00")));
+  }
+}
