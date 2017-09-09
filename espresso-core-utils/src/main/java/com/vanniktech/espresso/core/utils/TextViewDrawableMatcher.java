7f7512d1000f2d6787cde60674b7b453085f8df6
Niklas Baudy <niklas.baudy@vanniktech.de>
2017-09-10 01:37:04 +0200

    Increase testing coverage.

--- espresso-core-utils/src/main/java/com/vanniktech/espresso/core/utils/TextViewDrawableMatcher.java
+++ espresso-core-utils/src/main/java/com/vanniktech/espresso/core/utils/TextViewDrawableMatcher.java
@@ -11,19 +11,22 @@ import android.widget.TextView;
 import org.hamcrest.Description;
 
 import static android.os.Build.VERSION_CODES.JELLY_BEAN_MR1;
+import static com.vanniktech.espresso.core.utils.TextViewDrawableMatcher.Type.DRAWABLE_BOTTOM;
+import static com.vanniktech.espresso.core.utils.TextViewDrawableMatcher.Type.DRAWABLE_LEFT;
+import static com.vanniktech.espresso.core.utils.TextViewDrawableMatcher.Type.DRAWABLE_RELATIVE_BOTTOM;
+import static com.vanniktech.espresso.core.utils.TextViewDrawableMatcher.Type.DRAWABLE_RELATIVE_LEFT;
+import static com.vanniktech.espresso.core.utils.TextViewDrawableMatcher.Type.DRAWABLE_RELATIVE_RIGHT;
+import static com.vanniktech.espresso.core.utils.TextViewDrawableMatcher.Type.DRAWABLE_RELATIVE_TOP;
+import static com.vanniktech.espresso.core.utils.TextViewDrawableMatcher.Type.DRAWABLE_RIGHT;
+import static com.vanniktech.espresso.core.utils.TextViewDrawableMatcher.Type.DRAWABLE_TOP;
+import static com.vanniktech.espresso.core.utils.Utils.INDEX_BOTTOM;
+import static com.vanniktech.espresso.core.utils.Utils.INDEX_LEFT;
+import static com.vanniktech.espresso.core.utils.Utils.INDEX_RIGHT;
+import static com.vanniktech.espresso.core.utils.Utils.INDEX_TOP;
 import static com.vanniktech.espresso.core.utils.Utils.NO_DRAWABLE;
 import static com.vanniktech.espresso.core.utils.Utils.drawableMatches;
 
 public final class TextViewDrawableMatcher extends BoundedMatcher<View, TextView> {
-  private static final int DRAWABLE_LEFT = 0;
-  private static final int DRAWABLE_RELATIVE_LEFT = 4;
-  private static final int DRAWABLE_TOP = 1;
-  private static final int DRAWABLE_RELATIVE_TOP = 5;
-  private static final int DRAWABLE_RIGHT = 2;
-  private static final int DRAWABLE_RELATIVE_RIGHT = 6;
-  private static final int DRAWABLE_BOTTOM = 3;
-  private static final int DRAWABLE_RELATIVE_BOTTOM = 7;
-
   @CheckResult public static TextViewDrawableMatcher withTextViewDrawableLeft(@DrawableRes final int resourceId) {
     return new TextViewDrawableMatcher(resourceId, DRAWABLE_LEFT);
   }
@@ -89,76 +92,55 @@ public final class TextViewDrawableMatcher extends BoundedMatcher<View, TextView
   }
 
   private final int expectedId;
-  private final int identifier;
+  private final Type type;
 
-  private TextViewDrawableMatcher(final int expectedId, final int identifier) {
+  private TextViewDrawableMatcher(final int expectedId, final Type type) {
     super(TextView.class);
 
     this.expectedId = expectedId;
-    this.identifier = identifier;
+    this.type = type;
   }
 
   @Override protected boolean matchesSafely(final TextView textView) {
-    final Drawable drawable = getDrawable(textView);
-    return drawableMatches(textView, drawable, expectedId);
-  }
-
-  @SuppressLint("NewApi") private Drawable getDrawable(final TextView imageView) {
-    switch (identifier) {
-      case DRAWABLE_LEFT:
-        return imageView.getCompoundDrawables()[DRAWABLE_LEFT];
-      case DRAWABLE_RELATIVE_LEFT:
-        return imageView.getCompoundDrawablesRelative()[DRAWABLE_LEFT];
-      case DRAWABLE_TOP:
-        return imageView.getCompoundDrawables()[DRAWABLE_TOP];
-      case DRAWABLE_RELATIVE_TOP:
-        return imageView.getCompoundDrawablesRelative()[DRAWABLE_TOP];
-      case DRAWABLE_RIGHT:
-        return imageView.getCompoundDrawables()[DRAWABLE_RIGHT];
-      case DRAWABLE_RELATIVE_RIGHT:
-        return imageView.getCompoundDrawablesRelative()[DRAWABLE_RIGHT];
-      case DRAWABLE_BOTTOM:
-        return imageView.getCompoundDrawables()[DRAWABLE_BOTTOM];
-      case DRAWABLE_RELATIVE_BOTTOM:
-        return imageView.getCompoundDrawablesRelative()[DRAWABLE_BOTTOM];
-      default:
-        throw new AssertionError();
-    }
-  }
-
-  private String identifierAsString() {
-    switch (identifier) {
-      case DRAWABLE_LEFT:
-        return "left";
-      case DRAWABLE_RELATIVE_LEFT:
-        return "relative left";
-      case DRAWABLE_TOP:
-        return "top";
-      case DRAWABLE_RELATIVE_TOP:
-        return "relative top";
-      case DRAWABLE_RIGHT:
-        return "right";
-      case DRAWABLE_RELATIVE_RIGHT:
-        return "relative right";
-      case DRAWABLE_BOTTOM:
-        return "bottom";
-      case DRAWABLE_RELATIVE_BOTTOM:
-        return "relative bottom";
-      default:
-        throw new AssertionError();
-    }
+    return drawableMatches(textView, type.getDrawable(textView), expectedId);
   }
 
   @Override public void describeTo(final Description description) {
     if (expectedId == NO_DRAWABLE) {
       description.appendText("with no ")
-          .appendText(identifierAsString())
+          .appendText(type.string)
           .appendText(" drawable");
     } else {
       description.appendText("with ")
-          .appendText(identifierAsString())
+          .appendText(type.string)
           .appendText(" drawable from resource id: ")
           .appendValue(expectedId);
+    }
+  }
+
+  enum Type {
+    DRAWABLE_LEFT(INDEX_LEFT, false, "left"),
+    DRAWABLE_RELATIVE_LEFT(INDEX_LEFT, true, "relative left"),
+    DRAWABLE_TOP(INDEX_TOP, false, "top"),
+    DRAWABLE_RELATIVE_TOP(INDEX_TOP, true, "relative top"),
+    DRAWABLE_RIGHT(INDEX_RIGHT, false, "right"),
+    DRAWABLE_RELATIVE_RIGHT(INDEX_RIGHT, true, "relative right"),
+    DRAWABLE_BOTTOM(INDEX_BOTTOM, false, "bottom"),
+    DRAWABLE_RELATIVE_BOTTOM(INDEX_BOTTOM, true, "relative bottom");
+
+    private final int index;
+    private final boolean isRelative;
+    final String string;
+
+    Type(final int index, final boolean isRelative, final String string) {
+      this.index = index;
+      this.isRelative = isRelative;
+      this.string = string;
+    }
+
+    @SuppressLint("NewApi") public Drawable getDrawable(final TextView textView) {
+      final Drawable[] drawables = isRelative ? textView.getCompoundDrawablesRelative() : textView.getCompoundDrawables();
+      return drawables[index];
     }
   }
 }
