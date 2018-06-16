966f7ae692052d17004166423126c61304bc881c
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-06-16 20:06:00 +0200

    SetProgressAction to set the progress of a ProgressBar. (#47)

--- espresso-core-utils/src/androidTest/java/com/vanniktech/espresso/core/utils/SetProgressActionTest.java
+++ espresso-core-utils/src/androidTest/java/com/vanniktech/espresso/core/utils/SetProgressActionTest.java
@@ -2,30 +2,23 @@ package com.vanniktech.espresso.core.utils;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import junit.framework.AssertionFailedError;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.vanniktech.espresso.core.utils.ProgressMatcher.withProgress;
+import static com.vanniktech.espresso.core.utils.SetProgressAction.setProgress;
 import static com.vanniktech.espresso.core.utils.ProgressBarActivity.VIEW_ID;
 
-@RunWith(AndroidJUnit4.class) public final class ProgressMatcherTest {
-  @Rule public final ExpectedException expectedException = ExpectedException.none();
-
+@RunWith(AndroidJUnit4.class) public final class SetProgressActionTest {
   @Rule public final ActivityTestRule<ProgressBarActivity> activityTestRule = new ActivityTestRule<>(ProgressBarActivity.class);
 
-  @Test public void withProgressMatches() {
+  @Test public void settingProgress() {
     onView(withId(VIEW_ID)).check(matches(withProgress(1)));
-  }
-
-  @Test public void withProgressDoesNotMatch() {
-    expectedException.expect(AssertionFailedError.class);
-    expectedException.expectMessage("'has progress: <2>' doesn't match the selected view.");
-    onView(withId(VIEW_ID)).check(matches(withProgress(2)));
+    onView(withId(VIEW_ID)).perform(setProgress(95));
+    onView(withId(VIEW_ID)).check(matches(withProgress(95)));
   }
 }
