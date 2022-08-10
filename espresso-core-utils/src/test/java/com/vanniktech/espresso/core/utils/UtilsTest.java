69ce4dedce2c7a1b331b36a7c5b4316deeb7fb76
Niklas Baudy <niklas.baudy@vanniktech.de>
2022-08-10 13:32:36 +0200

    Refactoring: Remove assertj3 & privateconstructor for tests. (#86)

--- espresso-core-utils/src/test/java/com/vanniktech/espresso/core/utils/UtilsTest.java
+++ espresso-core-utils/src/test/java/com/vanniktech/espresso/core/utils/UtilsTest.java
@@ -1,27 +1,20 @@
 package com.vanniktech.espresso.core.utils;
 
-import com.pushtorefresh.private_constructor_checker.PrivateConstructorChecker;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import static com.vanniktech.espresso.core.utils.Utils.checkNotNull;
-import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public final class UtilsTest {
   @Rule public final ExpectedException expectedException = ExpectedException.none();
 
-  @Test public void constructorShouldBePrivate() {
-    PrivateConstructorChecker.forClass(Utils.class)
-        .expectedTypeOfException(AssertionError.class)
-        .expectedExceptionMessage("No instances.")
-        .check();
-  }
 
   @Test public void checkNotNullNoNull() {
-    assertThat(checkNotNull(5, "should never be")).isEqualTo(5);
-    assertThat(checkNotNull("5", "should never be")).isEqualTo("5");
-    assertThat(checkNotNull(3f, "should never be")).isEqualTo(3f);
+    assertEquals(Integer.valueOf(5), checkNotNull(5, "should never be"));
+    assertEquals("5", checkNotNull("5", "should never be"));
+    assertEquals(Float.valueOf(3f), checkNotNull(3f, "should never be"));
   }
 
   @Test public void checkNotNullNull() {
